93c4933f3c5d1a2828e8a30e38c864352b506cd8
mf-Q <loud_flamingo@hotmail.com>
2019-11-20 12:46:27 +0900

    add payjap_js-progressing
    サインアップ時のカード登録を作成中

--- app/assets/javascripts/credit.js
+++ app/assets/javascripts/credit.js
@@ -1,33 +1,38 @@
-document.addEventListener(
-  "DOMContentLoaded", (e) => {
-    Payjp.setPublicKey("公開鍵");
-    const btn = document.getElementById('token_submit'); //hamlで記載されているidを取得した時
-    btn.addEventListener("click", (e) => {　//formのボタンを押した時にイベント発動
-      e.preventDefault();
+$(document).on('turbolinks:load', function() {
+  var form = $("#card_form");                               // カード登録フォームに付与したidでフォームを指定
+  Payjp.setPublicKey('pk_test_b2b0be3df833644010a9ab94');   // pay.jpから提供されているテスト公開キー
+  
+  
+  $(document).on("click", "#token_submit", function(e) {    // sumbitボタンを押したらイベント発火
+    e.preventDefault();
+    form.find("input[type=submit]").prop("disabled", true); // sumbitボタンの2度押しをしない設定？
 
       //カード情報生成
       const card = {
-        number: document.getElementById("card_number").value,
-        cvc: document.getElementById("cvc").value,
-        exp_month: document.getElementById("month").value,
-        exp_year: document.getElementById("year").value
-      }; //入力されたデータの取得↑
+        number: parseInt($("#card_number").val()),          // parseIntなんか入力した値（文字列）を整数に変換して数値として取得してくれるメソッド？
+        cvc: parseInt($("#card_cvc").val()),
+        exp_month: parseInt($("#exp_month").val()),
+        exp_year: parseInt($("#exp_year").val())
+        };        //↑入力されたデータの取得、pay.jpに送るデータ
+
 
       //トークン生成
-      Payjp.createToken(card, (status, response) => {
-        if (status === 200) { //成功した場合
+      Payjp.createToken(card, function(status, response) {
+        if (stauts == 200) {                      // 出力（本来はサーバへ送信）:公式の記載がこうなってる。なんで200かがわからない…
           $("#card_number").removeAttr("name");
-          $("#cvc").removeAttr("name");
-          $("#month").removeAttr("name");
-          $("#year").removeAttr("name"); //カード情報を自分のサーバにpostせず削除します
-          $("#card_token").append(
-            $('<input type="hidden" name="payjp-token">').val(response.id)
-          ); //トークンを送信できるように隠しタグを生成
-          document.inputForm.submit();
-          alert("登録が完了しました"); //確認用
-        } else {
-          alert("カード情報が正しくありません。"); //確認用
+          $("#card_cvc").removeAttr("name");
+          $("#exp_month").removeAttr("name");
+          $("#exp_year").removeAttr("name");     //↑入力されたデータをこのアプリに保存しないようにフォームを空にする。
+  
+          var token = response.id;
+          $("#card_form").append($('<input type="hidden" name="payjp_token" class="payjp-token" />').val(token));   // 通信が成功しstatusが200になった時typeがhiddenとなっているinput要素が追加される
+          $("#card_form").get(0).submit();       //e.preventDefault();によって停止させていたsubmitを実行させる
+          alert("登録が完了しました。")
+        }
+        else {
+          alert("カード情報が正しくありません。")
+          form.find('button').prop('disabled', false);
         }
       });
     });
-  },false);
+  });
--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,12 +1,33 @@
 class CreditsController < ApplicationController
 
+  require "payjp"
+  before_action :entry_card
+
   def index
+
   end
   
   def show
   end
 
   def new
+    card = Card.where(user_id: current_user.id).first
+    redirect_to action: "index" if card.present?    # カードがなかったらcreateに飛ぶ
   end
   
+  def create
+    Payjp.api_key = 'sk_test_04387e0973780d6cbbb78c9e' #pay.jpの秘密キー
+
+    if params['payjp-token'].blank?
+        redirect_to action: "new"
+        customer = Payjp::Customer.create(description: 'test')  #顧客作成
+        customer.cards.create(card: params[:payjp_token])       #上の顧客のカード作成
+
+  end
+
+
+
+  def entry_card
+    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
+  end
 end
