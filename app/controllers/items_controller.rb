5e0fdae0e96fa617fedbb4e6e9310d2d1e42d571
ryoji25 <r25.k0305@gmail.com>
2019-11-25 15:26:40 +0900

    itemsコントローラー変数名変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,30 +1,29 @@
 class ItemsController < ApplicationController
 
   def index
-    @ladies_id = Category.find(1).subtree_ids
-    @ladies = Item.limit(10).where(category_id: @ladies_id)
+    @ladies_ids = Category.find(1).subtree_ids
+    @ladies = Item.limit(10).where(category_id: @ladies_ids)
 
+    @man_ids = Category.find(200).subtree_ids
+    @men = Item.limit(10).where(category_id: @man_ids)
 
-    @man_id = Category.find(200).subtree_ids
-    @men = Item.limit(10).where(category_id: @man_id)
+    @home_ids = Category.find(898).subtree_ids
+    @homes = Item.limit(10).where(category_id: @home_ids)
 
-    @home_id = Category.find(898).subtree_ids
-    @homes = Item.limit(10).where(category_id: @home_id)
+    @toy_ids = Category.find(685).subtree_ids
+    @toys = Item.limit(10).where(category_id: @toy_ids)
 
-    @toy_id = Category.find(685).subtree_ids
-    @toys = Item.limit(10).where(category_id: @toy_id)
+    @chanel_ids = Brand.find(1)
+    @chanels = Item.limit(10).where(brand_id: @chanel_ids)
 
-    @chanel_id = Brand.find(1)
-    @chanels = Item.limit(10).where(brand_id: @chanel_id)
+    @lv_ids = Brand.find(2)
+    @lvs = Item.limit(10).where(brand_id: @lv_ids)
 
-    @lv_id = Brand.find(2)
-    @lvs = Item.limit(10).where(brand_id: @lv_id)
-
-    @supreme_id = Brand.find(3)
-    @supremes = Item.limit(10).where(brand_id: @supreme_id)
+    @supreme_ids = Brand.find(3)
+    @supremes = Item.limit(10).where(brand_id: @supreme_ids)
     
-    @nike_id = Brand.find(4)
-    @nikes = Item.limit(10).where(brand_id: @nike_id)
+    @nike_ids = Brand.find(4)
+    @nikes = Item.limit(10).where(brand_id: @nike_ids)
 
   end
 
