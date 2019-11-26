bd282249080ad9284f8b76a757a6cffe873cf772
syota1025 <syota.kotera@gmail.com>
2019-11-26 18:51:59 +0900

    user.rbにバリデーションを記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,44 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  #ニックネームのバリデーション
+  validates :nickname, length: { minimum: 1, maximum: 30 }, on: :validates_registration
+
+  #性のバリデーション
+  validates :lastname, length: { minimum: 1, maximum: 30 }, on: :validates_registration
+  validates :lastname, length: { minimum: 1, maximum: 30 }, on: :validates_address
+
+  #名のバリデーション
+  validates :firstname, length: { minimum: 1, maximum: 30 }, on: :validates_registration
+  validates :firstname, length: { minimum: 1, maximum: 30 }, on: :validates_address
+
+  #性カナのバリデーション
+  validates :lastname_kana, length: { minimum: 1, maximum: 30 }, on: :validates_registration
+  validates :lastname_kana, length: { minimum: 1, maximum: 30 }, on: :validates_address
+
+  #名カナのバリデーション
+  validates :firstname_kana, length: { minimum: 1, maximum: 30 }, on: :validates_registration
+  validates :firstname_kana, length: { minimum: 1, maximum: 30 }, on: :validates_address
+
+  #生年月日のバリデーション
+  validates :birthday, length: { minimum: 8, maximum: 10 }, on: :validates_registration
+
+  #電話番号のバリデーション
+  validates :tel_number, length: { is: 11 }, on: :validates_authentication
+
+  #郵便番号のバリデーション
+  validates :postal_code, length: { is: 8 }, on: :validates_address
+
+  #都道府県のバリデーション
+  validates :prefecture_id, numericality: true, on: :validates_address
+
+  #市区町村のバリデーション
+  validates :city, length: { minimum: 1, maximum: 30 }, on: :validates_address
+  
+  #番地のバリデーション
+  validates :city_block, length: { minimum: 1, maximum: 30 }, on: :validates_address
+  
 end
