2bc4acfc7ee0e4740e50d5a6dfb63299506a92c7
kirikirio <kirikiri3926@gmail.com>
2019-11-27 19:35:01 +0900

    premitのsale.status削除他

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -62,12 +62,12 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name,:description,:delivery_fee,:price,:brand_id,:prefecture_id,:category_id,:size_id,:item_status_id,:sale_status_id,:delivery_status_id,:delivery_method_id,
-    images_attributes: [:id, :image]).merge(
-      name: 'パーカー', price: 3000, description: '商品の説明',
-      item_status_id: 1,size_id: 1,brand_id:1,
-      delivery_status_id: 1,prefecture_id: 1, category_id: 1,delivery_fee: 1,
-      user_id: 1, sale_status_id: 1,delivery_method_id: 1 )
+    params.require(:item).permit(:name,:description,:delivery_fee,:price,:brand_id,:prefecture_id,:category_id,:size_id,:item_status_id,:delivery_status_id,:delivery_method_id,
+    images_attributes: [:id, :image]).merge(user_id:1,delivery_method_id:1, category_id: 1,brand_id:1,size_id: 1, sale_status_id: 1)
+      # name: 'パーカー', price: 3000, description: '商品の説明',
+      # item_status_id: 1,size_id: 1,brand_id:1,
+      # delivery_status_id: 1,prefecture_id: 1, category_id: 1,delivery_fee: 1,
+      # user_id: 1, sale_status_id: 1,delivery_method_id: 1 )
   end
 
 end
