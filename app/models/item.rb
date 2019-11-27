32778e326d72faabf3f13190d782350d53f7c6de
kirikirio <kirikiri3926@gmail.com>
2019-11-27 17:57:27 +0900

    マスターで出品の際に画像が通らなかった問題を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,9 @@ class Item < ApplicationRecord
   belongs_to :delivery_status
   belongs_to :category
   belongs_to :delivery_method
-  has_many :comments, dependent: :destroy
   has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images, allow_destroy: true
+  has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   extend ActiveHash::Associations::ActiveRecordExtensions
