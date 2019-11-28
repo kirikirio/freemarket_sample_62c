2e85508405e8d97963540c9e60f9d80760d1b182
ryoji25 <r25.k0305@gmail.com>
2019-11-28 18:34:52 +0900

    出品中の商品をDBから取り出してビューに表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,29 @@ class ItemsController < ApplicationController
   before_action :set_submodel, only: [:new, :create]
 
   def index
+    @ladies_ids = Category.find(1).subtree_ids
+    @ladies = Item.limit(10).where(category_id: @ladies_ids)
+
+    @man_ids = Category.find(200).subtree_ids
+    @men = Item.limit(10).where(category_id: @man_ids)
+
+    @home_ids = Category.find(898).subtree_ids
+    @homes = Item.limit(10).where(category_id: @home_ids)
+
+    @toy_ids = Category.find(685).subtree_ids
+    @toys = Item.limit(10).where(category_id: @toy_ids)
+
+    @chanel_ids = Brand.find(1)
+    @chanels = Item.limit(10).where(brand_id: @chanel_ids)
+
+    @lv_ids = Brand.find(2)
+    @lvs = Item.limit(10).where(brand_id: @lv_ids)
+
+    @supreme_ids = Brand.find(3)
+    @supremes = Item.limit(10).where(brand_id: @supreme_ids)
+    
+    @nike_ids = Brand.find(4)
+    @nikes = Item.limit(10).where(brand_id: @nike_ids)
   end
 
   def new
