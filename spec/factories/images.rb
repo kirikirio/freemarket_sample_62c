a32a5f9c1ca048cdbca63609d8723602645943db
yu46 <nasu.y@hotmail.co.jp>
2019-11-28 12:36:49 +0900

    スペースの修正

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :image do
 
-    image               { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.png'))}
-    item_id              {1}
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.png'))}
+    item_id {1}
 
   end
 end
