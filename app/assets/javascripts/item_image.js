84768e260461811b471a1e53c878cb92e96c3e6e
yu46 <nasu.y@hotmail.co.jp>
2019-11-28 12:52:36 +0900

    修正

--- app/assets/javascripts/item_image.js
+++ app/assets/javascripts/item_image.js
@@ -2,17 +2,13 @@ $(function () {
   const imagePreview = (input) => {
     const reader = new FileReader();
     const file = input.files[0];
-    console.log(file)
 
     reader.onload = (e) => {
 
-      // console.log(e.target.result)
-      // $('#preview').attr('src', e.target.result)
       const html = `<img
               src=${e.target.result}
               class="preview-image"
               >`
-      // $('#preview').attr('src', e.target.result)
       $('.figure').empty();
       $('.figure').append(html);
     }
