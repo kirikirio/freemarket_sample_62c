f4404ba6e456f70fa62824ffc955d97447c10e20
kana uchimura <xxxxxx@gmail.com>
2019-11-29 19:43:34 +0900

    商品削除
    テスト

--- app/controllers/sellings_controller.rb
+++ app/controllers/sellings_controller.rb
@@ -1,11 +1,20 @@
 class SellingsController < ApplicationController
 
   def index
-    user = User.find(1)
+    user = User.find(current_user.id)
     @sellings = user.items.page(params[:page]).per(10).order("created_at DESC")
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
+  def edit
+  end
+  def destroy 
+    item = Item.find(params[:id])
+    item.destroy! if item.user_id == current_user.id
+    #destroyメソッドを使用し対象のツイートを削除する。
+    redirect_to user_sellings_path
+  end
 end
