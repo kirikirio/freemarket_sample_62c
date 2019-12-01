5405dee4e85259cb292cf4f9ebdc4a31b181b2bf
yu46 <nasu.y@hotmail.co.jp>
2019-12-01 16:16:43 +0900

    cssの修正

--- app/models/item_status.rb
+++ app/models/item_status.rb
@@ -1,13 +1,3 @@
 class ItemStatus < ApplicationRecord
   has_many :items​​
 end
-
-# 下記activehashの記述
-
-# class  ItemStatus < ActiveHash::Base
-  
-#   self.data = [
-#       {id: 1, name: '---'}, {id: 2, name: '新品'}, {id: 3, name: '未使用'},
-      
-#   ]
-# end
